8d6c76ae3f275a1612fd485204d8af9e596521c3
Anne Lopes <alexhumbertolds@gmail.com>
2020-10-06 20:50:37 -0300

    Alex's changes

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from James.");
+        System.out.println("Hello James, my name is Alex");
     }
 }
